a8d73f6526e9c9cd89c502a9435245b71002aa20
ken-diva <kendiva17@gmail.com>
2024-01-15 14:48:37 +0700

    fixing token 1

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import os
 
 # load token
 # load_dotenv()
-token = "6099300581:AAEMjnQN0gnieIcj029_tuXyNLi7NSAZt88"
-sheet_id = "1FUO33IdeaxgFnEfyb0ELkKo6fBBw5JyWH0zfdHH6Stg"
+# token = "6099300581:AAEMjnQN0gnieIcj029_tuXyNLi7NSAZt88"
+# sheet_id = "1FUO33IdeaxgFnEfyb0ELkKo6fBBw5JyWH0zfdHH6Stg"
+token = os.getenv("token", "")
+sheet_id = os.getenv("sheet_id", "")
 
 # You can set parse_mode by default. HTML or MARKDOWN
 # bot = telebot.TeleBot(token, parse_mode="HTML")
